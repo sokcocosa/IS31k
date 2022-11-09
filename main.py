4bd0cc53438a35f1414f72a4fc03628ea509a556
Airat <airatm@live.ru>
2022-11-09 23:45:17 +0500

    Интегрирован функционал первого генератора

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+from FirstRandom import CustomRandom as FirstPositive
+
 if __name__ == "__main__":
     print("1. Случайное целое положительное число от 0 до 100")
     print("2. Случайное целое отрицательное число от 0 до 100")
     v = input("Введите вариант генерации случайного числа: ")
-    match v:
+    match v.split():
+        case ['1']:
+            print(FirstPositive.positiveRandom())
         case _:
             print("Нет такого функционала")
