159d44f1871940e0a13ce19da127a7ba744b1bf3
lia-gat <114720640+lia-gat@users.noreply.github.com>
2022-11-10 11:18:56 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
-from FirstRandom import CustomRandom as FirstPositive
-from SecondRandom import CustomRandom as SecondNegative
+x = int(input("Enter x:\n"))
 
-if __name__ == "__main__":
-    print("1. Случайное целое положительное число от 0 до 100")
-    print("2. Случайное целое отрицательное число от 0 до 100")
-    v = input("Введите вариант генерации случайного числа: ")
-    match v.split():
-        case ['1']:
-            print(FirstPositive.positiveRandom())
-        case ['2']:
-            print(SecondNegative.negativeRandom())
-        case _:
-            print("Нет такого функционала")
+if x < 0:
+    y = -4
+elif x < 1 and x >= 0:
+    y = x * x + 3 * x + 4
+elif x >= 1:
+    y = 2
+
+print(y)
