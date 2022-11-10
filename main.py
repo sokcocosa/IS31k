2a61011ca42d8b59a50bb454fce4bc62fe05158d
Marsik2530 <marselakbar285@gmail.com>
2022-11-10 18:05:24 +0500

    Marsik2530

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
-from FirstRandom import CustomRandom as FirstPositive
-from SecondRandom import CustomRandom as SecondNegative
-
-if __name__ == "__main__":
-    print("1. Случайное целое положительное число от 0 до 100")
-    print("2. Случайное целое отрицательное число от 0 до 100")
-    v = input("Введите вариант генерации случайного числа: ")
-    match v.split():
-        case ['1']:
-            print(FirstPositive.positiveRandom())
-        case ['2']:
-            print(SecondNegative.negativeRandom())
-        case _:
-            print("Нет такого функционала")
+from random import randint
+n = int(input("Введите длину динамического массива : "))
+a = [[randint(0,100)
+     for i in range(n)]]
+b = [[randint(0,100)
+     for i in range(n)]]  
+s = 0
+r = 0
+for i in range(len(a)):
+    for j in range(len(a[i])):
+        s += a[i][j]
+for i in range(len(b)):
+    for j in range(len(b[i])):
+        s +=b[i][j]
+print(s)
+for i in range(len(a)):
+    for j in range(len(a[i])):
+        r-= a[i][j]
+for i in range(len(b)):
+    for j in range(len(b[i])):
+        r -=b[i][j]
+print(r)
