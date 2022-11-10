893d054fbaf3d3a4ddc8587b678d8261d7bd8d01
GostenovIM and BeloglazovLM <BeloglazovLev1@mail.ru>
2022-11-10 12:42:16 +0500

    из большего вычитает меньшее

--- 4 вариант.py
+++ 4 вариант.py
@@ -1,5 +1,15 @@
-a, b = int(input()), int(input())
-if a > b:
-    print(a - b)
-elif b > a:
-    print(b - a)
+import random
+Variant4=int(input("Матрица: "))
+ggg=[[random.randint(1,9) for i in range(Variant4)] for j in range(Variant4)]
+min = ggg[0][0]
+for i in range(Variant4):
+    for j in range(Variant4):
+        if(ggg[i][j]<min): min=ggg[i][j]
+for i in range(Variant4): print(ggg[i])
+max = ggg[0][0]
+for i in range(Variant4):
+    for j in range(Variant4):
+        if(ggg[i][j]>max): max=ggg[i][j]
+print(min)
+print(max)
+print(max - min)
