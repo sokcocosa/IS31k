6f4a65374d0776fba15f1d835bcaf39a4b1c1942
YanGrov4 <113303162+YanGrov4@users.noreply.github.com>
2022-11-10 10:16:09 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#Вариант 12
+#Вариант 12 - вычислить определитель матрицы (3x3,2x2) 
 
 def opredelitil_3x3(list1):
     return list1[0][0] * list1[1][1] * list1[2][2] + list1[0][1] * list1[1][2] * list1[2][0] + list1[0][2] * list1[1][0] * list1[2][1] - list1[2][0] * list1[1][1] * list1[0][2] - list1[2][1] * list1[1][2] * list1[0][0] - list1[2][2] * list1[1][0] * list1[0][1]
