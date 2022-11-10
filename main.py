bfeaf6fc5e1c230215e504aaad7a16876796ec9e
2 <2@nfbashgu.local>
2022-11-10 12:39:13 +0500

    fix

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
-from FirstRandom import CustomRandom as FirstPositive
-from SecondRandom import CustomRandom as SecondNegative
+import random
 
-if __name__ == "__main__":
-    print("1. Случайное целое положительное число от 0 до 100")
-    print("2. Случайное целое отрицательное число от 0 до 100")
-    v = input("Введите вариант генерации случайного числа: ")
-    match v.split():
-        case ['1']:
-            print(FirstPositive.positiveRandom())
-        case ['2']:
-            print(SecondNegative.negativeRandom())
-        case _:
-            print("Нет такого функционала")
+X = [[random.randint(0, 10),random.randint(0, 10),random.randint(0, 10)],
+     [random.randint(0, 10),random.randint(0, 10),random.randint(0, 10)],
+     [random.randint(0, 10),random.randint(0, 10),random.randint(0, 10)]]
+
+Y = [[random.randint(0, 10),random.randint(0, 10),random.randint(0, 10)],
+     [random.randint(0, 10),random.randint(0, 10),random.randint(0, 10)],
+     [random.randint(0, 10),random.randint(0, 10),random.randint(0, 10)]]
+
+result = [[0,0,0],[0,0,0],[0,0,0]]
+
+for i in range(len(X)):
+   for j in range(len(X[0])):
+        result[i][j] = X[i][j] + Y[i][j]
+print("Матрица X:")
+for X1 in X:
+      print(X1)
+print("==================================")
+print("Матрица Y:")
+for Y1 in Y:
+      print(Y1)
+print("==================================")
+print("Сумма матриц X и Y:")
+for r in result:
+      print(r)
