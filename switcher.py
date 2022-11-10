929f426d411e858afb88e206b0e63815988a4b45
Un Couteau <5453712-un_couteau@users.noreply.gitlab.com>
2022-11-10 13:51:55 +0500

    fix

--- switcher.py
+++ switcher.py
@@ -8,7 +8,10 @@ if __name__ == "__main__":
     if choice in lst:
         if os.system(f"git switch {choice}") == 0:
             print(f"\033[33mИдет выполнение работы\033[0m \033[31m{choice}\033[33m варианта\033[0m")
-            os.system("python main.py")
+            if os.path.exists("main.py"):
+                os.system("python main.py")
+            else:
+                print("\033[31mОшибка!\033[0m\nНе найден \033[33mmain.py\033[0m")
         else:
             print("\033[31mОшибка!\033[0m\nВетка не была найдена. Попробуйте обновить командой \033[33mgit pull\033[0m")
     else:
