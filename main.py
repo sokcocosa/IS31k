dd61c2435680bb78aa85d728c75d3b5ffd4bad9e
FFF <fff@example.com>
2022-11-10 09:55:06 +0500

    python code

---
+++ main.py
@@ -0,0 +1,8 @@
+#Вариант 12
+
+def opredelitil_3x3(list1):
+    return list1[0][0] * list1[1][1] * list1[2][2] + list1[0][1] * list1[1][2] * list1[2][0] + list1[0][2] * list1[1][0] * list1[2][1] - list1[2][0] * list1[1][1] * list1[0][2] - list1[2][1] * list1[1][2] * list1[0][0] - list1[2][2] * list1[1][0] * list1[0][1]
+def opredelitil_2x2(list1):
+    return list1[0][0] * list1[1][1] - list1[0][1] * list1[1][0]
+
+
