6c81e62ad73f13b66ffb6a3ec89805af6f8ead18
YanGrov4 <113303162+YanGrov4@users.noreply.github.com>
2022-11-10 12:10:27 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
-#Вариант 12 - вычислить определитель матрицы (3x3,2x2) 
+#Вариант 12
 
-def opredelitil_3x3(list1):
-    return list1[0][0] * list1[1][1] * list1[2][2] + list1[0][1] * list1[1][2] * list1[2][0] + list1[0][2] * list1[1][0] * list1[2][1] - list1[2][0] * list1[1][1] * list1[0][2] - list1[2][1] * list1[1][2] * list1[0][0] - list1[2][2] * list1[1][0] * list1[0][1]
+
+def oprdelitil_3x3(list1):
+    try:
+        return list1[0][0] * list1[1][1] * list1[2][2] + list1[0][1] * list1[1][2] * list1[2][0] + list1[0][2] * list1[1][0] * list1[2][1] - \
+               list1[2][0] * list1[1][1] * list1[0][2] - list1[2][1] * list1[1][2] * list1[0][0] - list1[2][2] * list1[1][0] * list1[0][1]
+    except IndexError:
+        return "Нужна матрица 3x3"
+
+
 def opredelitil_2x2(list1):
-    return list1[0][0] * list1[1][1] - list1[0][1] * list1[1][0]
+    try:
+        return list1[0][0] * list1[1][1] - list1[0][1] * list1[1][0]
+    except IndexError:
+        print("Нужна матрица 2x2")
 
 
+numb = int(input())
+list1 = []
+if numb == 3:
+    for i in range(3):
+        list1.append(list(map(int, input().split())))
+        print(list1)
+    print(oprdelitil_3x3(list1))
+elif numb == 2:
+    for i in range(2):
+        list1.append(list(map(int, input().split())))
+    print(opredelitil_2x2(list1))
