379860382d337566a7ba529cb08806a413751a0e
студент НФ БашГУ <@nfbashgu.local>
2022-11-17 14:54:27 +0500

    i

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
-from FirstRandom import CustomRandom as FirstPositive
-from SecondRandom import CustomRandom as SecondNegative
+a = int(input('Напиши a '))
+b = int(input('Напиши b '))
+c = int(input('Напиши c '))
+print(a, '* x^2 +', b, '* x +', c, '= 0')
+D = b ** 2 - 4 * a * c
 
-if __name__ == "__main__":
-    print("1. Случайное целое положительное число от 0 до 100")
-    print("2. Случайное целое отрицательное число от 0 до 100")
-    v = input("Введите вариант генерации случайного числа: ")
-    match v.split():
-        case ['1']:
-            print(FirstPositive.positiveRandom())
-        case ['2']:
-            print(SecondNegative.negativeRandom())
-        case _:
-            print("Нет такого функционала")
+if a == 0 and b == 0:
+    print('Неразрешаемое уравнение')
+elif a == 0:
+    print('Неквадратное уравнение')
+elif D > 0:
+    x1 = (-b + D ** (1 / 2)) / (2 * a)
+    x2 = (-b - D ** (1 / 2)) / (2 * a)
+    print('x1 =', x1)
+    print('x2 =', x2)
+    print('Случай вещесвенных корней')
+elif D == 0:
+    x = (-b) / (2 * a)
+
+    print('x2 = 0')
+    print('Нулевые корни')
+else:
+    print("Случай комплексных корней")
